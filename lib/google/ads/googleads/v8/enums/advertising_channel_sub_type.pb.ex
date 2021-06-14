f95a149965db580e58750bdabdb6fa553c90bc10
zvonevbt <zvonimir.ivaskovic@verybgthings.com>
2021-06-14 10:54:19 +0200

    Transform  advertising channel sub type to proto2

--- lib/google/ads/googleads/v8/enums/advertising_channel_sub_type.pb.ex
+++ lib/google/ads/googleads/v8/enums/advertising_channel_sub_type.pb.ex
@@ -1,6 +1,6 @@
 defmodule Google.Ads.Googleads.V8.Enums.AdvertisingChannelSubTypeEnum.AdvertisingChannelSubType do
   @moduledoc false
-  use Protobuf, enum: true, syntax: :proto3
+  use Protobuf, enum: true, syntax: :proto2
 
   @type t ::
           integer
@@ -62,7 +62,7 @@ end
 
 defmodule Google.Ads.Googleads.V8.Enums.AdvertisingChannelSubTypeEnum do
   @moduledoc false
-  use Protobuf, syntax: :proto3
+  use Protobuf, syntax: :proto2
   @type t :: %__MODULE__{}
 
   defstruct []
