aa1ae24a90b8b2ccb756412bef6b911a2ef25c1d
zvlasic <zlayo1@gmail.com>
2023-06-23 12:44:15 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule GoogleAds.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:grpc, github: "VeryBigThings/grpc", branch: "elixir-grpc-master-2"}
+      {:grpc, github: "VeryBigThings/grpc", branch: "elixir-grpc-master-3"}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
