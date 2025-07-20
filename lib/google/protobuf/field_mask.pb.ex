a860e7dd11d051f9de8a79fa0d66193ae00dceee
ken-kost <kenneth.kostresevic@gmail.com>
2025-07-20 15:35:04 +0200

    Comment out field mask module

--- lib/google/protobuf/field_mask.pb.ex
+++ lib/google/protobuf/field_mask.pb.ex
@@ -1,6 +1,6 @@
-defmodule Google.Protobuf.FieldMask do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
+# defmodule Google.Protobuf.FieldMask do
+#   @moduledoc false
+#   use Protobuf, syntax: :proto3
 
-  field(:paths, 1, repeated: true, type: :string)
-end
+#   field(:paths, 1, repeated: true, type: :string)
+# end
